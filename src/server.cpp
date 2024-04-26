969e7991162e3922b814fc864f5ee328d7681f41
fenbf <joebaf@gmail.com>
2024-04-26 09:46:41 +0200

    2nd stage, reworked some existing code example

--- src/server.cpp
+++ src/server.cpp
@@ -49,9 +49,21 @@ int main(int argc, char **argv) {
   
   std::cout << "Waiting for a client to connect...\n";
   
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
   
+  char buffer[255] { 0 };
+  const int len = read(client_fd, buffer, 255);
+
+  if (len < 0)
+    std::cout << "Failed to read the request\n";
+
+  std::cout << "resd buffer:\n" << buffer << std::endl;
+
+  if (write(client_fd, "HTTP/1.1 200 OK\r\n\r\n", 19) < 0)
+		std::cout << "Failed to write the request\n";
+
+  std::cout << "Message send" << std::endl;
   close(server_fd);
 
   return 0;
